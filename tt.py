131add4c94f7a1b388b72eb9d542f43c1996e151
koung1500 <152825159+koung1500@users.noreply.github.com>
2023-12-11 11:23:04 +0900

    Add files via upload

--- tt.py
+++ tt.py
@@ -17,11 +17,11 @@ import numpy as np
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    c1 = st.radio('선의 색을 선택하시오',['red', 'green', 'blue', 'orange'])
+    c1 = st.sidebar.radio('선의 색을 선택하시오',['red', 'green', 'blue', 'orange'])
 with col2:
-    s1 = st.radio('선의 형태을 선택하시오', ['-', ':', '--'])
+    s1 = st.sidebar.radio('선의 형태을 선택하시오', ['-', ':', '--'])
 with col3:
-    m1 = st.radio('마커의 형태를 선택하시오',['o', '^', 's', 'p'])
+    m1 = st.sidebar.radio('마커의 형태를 선택하시오',['o', '^', 's', 'p'])
     
 fig, ax = plt.subplots()
 
